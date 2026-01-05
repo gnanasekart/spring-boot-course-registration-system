61324b8fb88736d4ca2ff9023693626c47bb446d
gnana sekar <gnanasekar69@hotmail.com>
2026-01-05 18:53:44 +0530

    Initial commit

---
+++ src/main/java/com/example/spring/boot/course/registration/system/SpringBootCourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.course.registration.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/boot/course/registration/system/SpringBootCourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.course.registration.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
